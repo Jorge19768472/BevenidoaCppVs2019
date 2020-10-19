3dd9f1eebf6be4a43f718f28bfbb06faa4dc071d
jorge <geordi_j_laforge@hotmail.com>
2020-10-19 19:36:14 +0200

    Agregar archivos de proyecto.

---
+++ BevenidoaCppVs2019.cpp
@@ -0,0 +1,50 @@
+// BevenidoaCppVs2019.cpp : Este archivo contiene la función "main". La ejecución del programa comienza y termina ahí.
+//
+
+#include <iostream>
+
+// para no tener que utilizar std::
+using namespace std;
+
+int main()
+
+{ 
+	cout << "! Bienvenida a C++   progrma de triangulos !\n";
+	int a, b, c;
+
+	cout << "\n ingrese el lado a: ";
+	cin >> a;
+
+	cout << "\n ingrese el lado b: ";
+	cin >> b;
+
+	cout << "\n ingrese el lado c: ";
+	cin >> c;
+
+	if (a + b < c || a + c < b || b + c < a)
+	{
+		cout << "\n No forma un triangulo";
+	}
+
+	else
+
+	{
+		if (a==b&&a==c&&b==c)
+		{ 
+
+		}
+	
+
+	}
+
+}
+
+// Ejecutar programa: Ctrl + F5 o menú Depurar > Iniciar sin depurar
+// Depurar programa: F5 o menú Depurar > Iniciar depuración
+
+// Sugerencias para primeros pasos: 1. Use la ventana del Explorador de soluciones para agregar y administrar archivos
+//   2. Use la ventana de Team Explorer para conectar con el control de código fuente
+//   3. Use la ventana de salida para ver la salida de compilación y otros mensajes
+//   4. Use la ventana Lista de errores para ver los errores
+//   5. Vaya a Proyecto > Agregar nuevo elemento para crear nuevos archivos de código, o a Proyecto > Agregar elemento existente para agregar archivos de código existentes al proyecto
+//   6. En el futuro, para volver a abrir este proyecto, vaya a Archivo > Abrir > Proyecto y seleccione el archivo .sln
